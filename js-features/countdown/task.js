2b6e45a7bfefe6bbfda5c31dda960c3631aa5539
Smelti <164363091+Smelti@users.noreply.github.com>
2024-11-28 14:26:17 +0300

    Update task.js

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -4,7 +4,7 @@ let time = 59;
    function updateTimer(){
     const hour = Math.floor(time / 3600)
     const minutes = Math.floor(time / 60);
-    const seconds = time % 60
+    const seconds = time % 60;
     timerElement.textContent = String(hour).padStart(2,'0') + ":" + String(minutes).padStart(2,'0') + ":" + String(seconds).padStart(2,'0'); 
      
     if(time <= 0) {
