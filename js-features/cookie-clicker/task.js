1e43b8a9936ab35c0aee66ecc4481a77614c4eec
Smelti <164363091+Smelti@users.noreply.github.com>
2024-11-28 14:22:06 +0300

    click.js

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,18 @@
+const image = document.getElementById('cookie');
+const clickCount = document.getElementById('clicker__counter');
+let count = 0;
+let cookieClick = false;
+image.addEventListener('click', () => {
+  count++;
+  clickCount.textContent = count;
+  
+  if (cookieClick) {
+   image.style.width = '210px'
+   image.style.height = '140px'
+  };
+  else {
+    image.style.width = '200px'
+    image.style.height = '130px'
+  };
+  cookieClick = !cookieClick
+});
