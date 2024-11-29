e914c4e0b6dfac1c5ab3e0bab7de5c39a89d0332
Smelti <164363091+Smelti@users.noreply.github.com>
2024-11-30 01:02:25 +0300

    Update task.js

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,16 +17,19 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
-  }
-
+   document.addEventListener('keydown', (event) => {
+     const input = event.key.toLowerCase();
+     const current = this.currentSymbol.textContent.toLowerCase();
+     
+     if(input === current) {
+       this.success()
+     }
+     else {
+       this.fail();
+     };
+   });
+  };
+  
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
     this.currentSymbol.classList.add('symbol_correct');
