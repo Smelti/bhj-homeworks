783854c8686396bf189895195069c65c24f36c2d
Smelti <164363091+Smelti@users.noreply.github.com>
2024-11-30 01:25:31 +0300

    Update task.js

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+const tabGroups = document.querySelectorAll('.tabs')
+tabGroups.forEach(group => {
+  const tabs = group.querySelectorAll('.tab')
+  const content = group.querySelectorAll('.tab__content')
+  
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(tab => tab.classList.remove('tab_active'))
+      
+      content.forEach(content => content.classList.remove('tab__content_active'))
+      
+      content[index].classList.add('tab__content_active')
+    })
+  })  
+})
