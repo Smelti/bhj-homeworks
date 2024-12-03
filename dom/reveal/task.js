4ade59e24130687af57da79b34da3fb313a256c4
Smelti <164363091+Smelti@users.noreply.github.com>
2024-12-03 22:10:08 +0300

    Update task.js

---
+++ dom/reveal/task.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll('.reveal')
+
+function checkVisibility() {
+  reveals.forEach(reveal => {
+    const rect = reveal.getBoundingClientRect()
+    const isVisible = rect.top < window.innerHeight && rect.bottom > 0;
+    
+    if (isVisible) {
+      reveal.classList.add('reveal_active')
+    }
+    else {
+      reveal.classList.remove('reveal_active')
+    }
+  })
+}
+window.addEventListener('scroll', checkVisibility)
+checkVisibility()
